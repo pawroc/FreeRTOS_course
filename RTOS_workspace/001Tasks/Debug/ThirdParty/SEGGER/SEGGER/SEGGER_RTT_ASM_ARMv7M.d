825860f2d436a9aba707d05f0147c0324569cf36
Pawel Drozdz <paweldrozdz.91@gmail.com>
2022-02-19 21:32:12 +0100

    Fixed STM32CubeIDE workspace

--- RTOS_workspace/001Tasks/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- RTOS_workspace/001Tasks/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
